56b6019f47b580b32251550ba41a06f4169cba18
Nathan Demick <ndemick@gmail.com>
2016-05-19 11:57:44 -0400

    Formatting updates; also file rename

--- comments.js
+++ comments.js
@@ -1,17 +1,18 @@
-(function (root) {
+(function () {
     /**
      * @constructor
      */
     var CommentHelper = function () {
+        this.slug = window.location.href.split('/').pop();
+        this.url = 'https://nathandemick.com/api'; // Replace this w/ ur server
+
+        this.writeForm();
+
         // Load reCAPTCHA
         var script = document.createElement('script');
         script.src = 'https://www.google.com/recaptcha/api.js';
         document.head.appendChild(script);
 
-        this.slug = window.location.href.split('/').pop();
-        this.url = 'http://localhost:4567'; // Replace this w/ ur server
-
-        this.writeForm();
         this.getComments();
     };
 
@@ -20,16 +21,17 @@
         var html = [
             '<div id="comments">',
             '<div id="comment_list"></div>',
-            '<form id="comment_form" action="' + this.url + '" method="POST">',
+            '<div id="comment_error_msg">There was a problem communicating with the comment server.</div>',
+            '<form id="comment_form">',
             '<input type="hidden" name="slug" value="' + this.slug + '">',
-            '<label for="comment_author">Your name:</label>',
-            '<input id="comment_author" type="text" name="author" required="required">',
-            '<label for="comment_url">Your website:</label>',
-            '<input id="comment_url" type="text" name="url">',
-            '<label for="comment_body">Your comment:</label>',
-            '<textarea id="comment_body" name="body" required="required"></textarea>',
+            '<p><label for="comment_author">Name</label>',
+            '<input id="comment_author" type="text" name="author" required="required"></p>',
+            '<p><label for="comment_url">Website</label>',
+            '<input id="comment_url" type="url" name="url"></p>',
+            '<p><label for="comment_body">Comment</label>',
+            '<textarea id="comment_body" name="body" required="required"></textarea></p>',
             '<div class="g-recaptcha" data-sitekey="6LeqwR8TAAAAAKl3dbC6494ll8Eki6ss5zBgWxxw"></div>',
-            '<button>Submit</button>',
+            '<button>Send!</button>',
             '</form>',
             '</div> <!-- /#comments -->'
         ].join('');
@@ -42,6 +44,8 @@
         this.commentUrl = document.getElementById('comment_url');
         this.commentBody = document.getElementById('comment_body');
         this.commentForm = document.getElementById('comment_form');
+        this.commentErrorMessage = document.getElementById('comment_error_msg');
+        this.hideError();
 
         this.commentForm.addEventListener('submit', this.postComment.bind(this));
     }
@@ -53,12 +57,13 @@
             authorName = '<a href="' + context.url + '" rel="nofollow">' + authorName + '</a>';
         }
 
+        var dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
         var element = document.createElement('section');
         element.className = 'comment';
         element.innerHTML = [
-            '<span class="name">' + authorName + '</span>',
-            '<span class="timestamp">' + context.timestamp + '</span>',
-            '<span class="body">' + context.body + '</span>',
+            '<p><span class="name">' + authorName + '</span> wrote on ',
+            '<span class="timestamp">' + new Date(context.timestamp * 1000).toLocaleDateString('en-US', dateOptions) + '</span>:</p>',
+            '<p class="body">' + context.body + '</p>',
         ].join('');
 
         return element;
@@ -75,7 +80,7 @@
                 try {
                     var response = JSON.parse(request.responseText);
 
-                    response.forEach(function (params) {
+                    response.comments.forEach(function (params) {
                         this.commentList.appendChild(this.commentTemplate(params));
                     }.bind(this));
                 } catch (e) {
@@ -93,12 +98,15 @@
     CommentHelper.prototype.postComment = function (event) {
         event.preventDefault();
 
+        this.hideError();
+
         var params = {
             slug: this.slug,
             author: this.commentAuthor.value,
             url: this.commentUrl.value,
             body: this.commentBody.value,
-            timestamp: Date.now() / 1000
+            timestamp: Date.now() / 1000,
+            'g-recaptcha-response': document.getElementById('g-recaptcha-response').value
         };
 
         var serializedParams = Object.keys(params).map(function (key) {
@@ -107,12 +115,14 @@
 
         var request = new XMLHttpRequest();
         request.onreadystatechange = function (event) {
-            if (this.readyState !== 4) {
+            if (request.readyState !== 4) {
                 return;
             }
 
             if (Math.floor(request.status / 100) === 2) {
                 this.commentList.appendChild(this.commentTemplate(params));
+                this.commentForm.reset();
+                grecaptcha.reset();
             } else {
                 this.showError();
             }
@@ -123,8 +133,13 @@
     };
 
     CommentHelper.prototype.showError = function () {
-        console.warn('Implement this');
+        this.commentErrorMessage.style.display = 'block';
     };
 
-    root.CommentHelper = CommentHelper;
-}(window));
+    CommentHelper.prototype.hideError = function () {
+        this.commentErrorMessage.style.display = 'none';
+    };
+
+    // Kick it off!
+    var comments = new CommentHelper();
+}());
